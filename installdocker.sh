46524913fe1c6263215915b87d51cb273b4eefef
wangyiran125 <wangyiran125@icloud.com>
2017-12-12 21:46:16 +0800

    add maven scala alpine dockerfile

---
+++ installdocker.sh
@@ -0,0 +1,10 @@
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://ce714c86.m.daocloud.io
+# step 1: 安装必要的一些系统工具
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+# Step 2: 添加软件源信息
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+# Step 3: 更新并安装 Docker-CE
+sudo yum makecache fast
+sudo yum -y install docker-ce
+# Step 4: 开启Docker服务
+sudo service docker start
